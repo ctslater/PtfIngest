c6566fe79a856e0ec493bda9b395cc9abb11de74
Colin Slater <ctslater@uw.edu>
2017-07-24 15:14:21 -0700

    Successfully ingested files. Checkpoint before cleanup.

---
+++ src/test/scala/ptf/PtfBenchmark.scala
@@ -0,0 +1,31 @@
+
+package ptfBenchmark
+
+import scala.util.Random
+import ptf.PtfIngest.match_within_zone
+import org.openjdk.jmh.annotations.Benchmark
+import ptf.{CoordsIdZone, SrcObjMatches}
+
+
+class ObjectMatchingBenchmark {
+
+  @Benchmark
+  def runMatching: Iterable[SrcObjMatches] = {
+
+    val zone = 5
+    val rows: Seq[CoordsIdZone] = randomPoints
+    match_within_zone(zone, rows.toIterator)
+  }
+
+  def randomPoints: Seq[CoordsIdZone] = {
+    val rand = new Random
+    val raDec = (1 to 1000).map{ _ => (rand.nextFloat(), rand.nextFloat()) }
+    (1 to 100).flatMap { visit =>
+      raDec.map { case (ra, dec) =>
+        CoordsIdZone(source_id=rand.nextLong(), visit=visit,
+                    ALPHAWIN_J2000=ra, DELTAWIN_J2000=dec, zone=5)
+      }
+    }
+  }
+
+}
